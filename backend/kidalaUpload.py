97d1192116cac288582defe661e88bc2f8a555d8
Piparkuucinsh <audaja107@gmail.com>
2022-05-08 20:05:45 +0300

    fixed ble

--- backend/kidalaUpload.py
+++ backend/kidalaUpload.py
@@ -5,31 +5,34 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+
+UPLOAD_FOLDER = app.root_path + '\\files'
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World</p>"
 
 @app.route("/<filename>")
 def downloadFile(filename):
-    return send_file(os.path.join(r"C:\Users\Piparkuucinsh\Desktop\mantas8\kidala-upload\files", filename), as_attachment=True)
+    return send_file(os.path.join(UPLOAD_FOLDER, filename), as_attachment=True)
 
 @app.route('/upload', methods=['POST'])
 def upload():
 
     if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            return {'msg': "failed"}
 
     file = request.files['file']
     print(file.filename)
     if file.filename == '':
             flash('No selected file')
-            return redirect(request.url)
+            return {'msg': "failed"}
     if file:
-        file.save(os.path.join(r"C:\Users\Piparkuucinsh\Desktop\mantas8\kidala-upload\files", file.filename).replace("PNG", "png"))
-        return f"localhost:5000/{file.filename}"
+        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+        return {'msg': "success", 'url': f"http://localhost:5000/{file.filename}"}
 
-    return "poh"
+    return {'msg': "failed"}
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000, debug=False)
