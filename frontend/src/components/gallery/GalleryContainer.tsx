1b688341d1144d1cfa00eef2a751dcd296ebbc6f
Rob <robzlegz@gmail.com>
2022-08-14 01:09:55 +0300

    gallery update

--- frontend/src/components/gallery/GalleryContainer.tsx
+++ frontend/src/components/gallery/GalleryContainer.tsx
@@ -4,7 +4,7 @@ import GalleryImages from './GalleryImages';
 
 function GalleryContainet() {
     return (
-        <div className="w-full bg-orange-500 min-h-screen pt-10">
+        <div className="w-full bg-orange-500 min-h-screen pt-10 px-2">
             <Link href="/">
                 <p className="text-blue-300 hover:underline focus:underline">
                     Upload more
