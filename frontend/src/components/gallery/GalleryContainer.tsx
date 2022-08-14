a49eac865bc773b5ab6610e327ee1ac7f80595d3
Rob <robzlegz@gmail.com>
2022-08-14 09:17:56 +0300

    typo

--- frontend/src/components/gallery/GalleryContainer.tsx
+++ frontend/src/components/gallery/GalleryContainer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import GalleryImages from './GalleryImages';
 
-function GalleryContainet() {
+function GalleryContainer() {
     return (
         <div className="w-full min-h-screen pt-10 px-2">
             <Link href="/">
@@ -16,4 +16,4 @@ function GalleryContainet() {
     );
 }
 
-export default GalleryContainet;
+export default GalleryContainer;
