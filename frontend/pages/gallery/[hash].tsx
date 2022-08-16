94e12b798b5034561ad71748fe46c48372a9e9fb
RobzLegz <robzlegz@gmail.com>
2022-08-16 16:20:00 +0300

    og image

--- frontend/pages/gallery/[hash].tsx
+++ frontend/pages/gallery/[hash].tsx
@@ -7,14 +7,13 @@ export default function Home() {
             <Head>
                 <title>Kidala upload | Share file</title>
                 <meta
-                    name="description"
-                    content="Kidala life. Max kidala. Safe pacans. 🔥 stafaars. Max safe pacani utt. Only at kidala-upload.vercel.app. Kidala hosting services. 🌐🤙🏿🅿😱 KEEP CALM AND BE KIDALA"
-                />
-                <meta
                     content="/images/janisbataragsuzliso.png"
                     property="og:image"
                 />
-                <link rel="/images/janisbataragsuzliso.png" />
+                <meta
+                    name="description"
+                    content="Kidala life. Max kidala. Safe pacans. 🔥 stafaars. Max safe pacani utt. Only at kidala-upload.vercel.app. Kidala hosting services. 🌐🤙🏿🅿😱 KEEP CALM AND BE KIDALA"
+                />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
