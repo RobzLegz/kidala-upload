9a863c389450732d54c9b567961d133a44bab8c8
Rob <robzlegz@gmail.com>
2022-08-16 10:47:26 +0300

    img fix

--- frontend/src/components/gallery/GalleryImage.tsx
+++ frontend/src/components/gallery/GalleryImage.tsx
@@ -10,13 +10,28 @@ const GalleryImage: React.FC<{ file: FileInterface }> = ({ file }) => {
     const router = useRouter();
 
     const [isLeft, setIsLeft] = useState(true);
+    const [isTop, setIsTop] = useState(true);
 
     const checkPosition = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        const { width } = windowSize;
+        const { width, height } = windowSize;
 
         if (width) {
             if (e.clientX > width / 2) {
                 setIsLeft(false);
+            }
+
+            if (height) {
+                let imgHeight = 0;
+
+                if (width >= 1536) {
+                    imgHeight = 600;
+                } else {
+                    imgHeight = 384;
+                }
+
+                if(e.clientY + imgHeight > height){
+                    setIsTop(false);
+                }
             }
         }
     };
@@ -55,9 +70,9 @@ const GalleryImage: React.FC<{ file: FileInterface }> = ({ file }) => {
             />
 
             <div
-                className={`hidden sm:group-hover:flex absolute top-0 lg:w-[600px] z-10 ${
+                className={`hidden sm:group-hover:flex absolute lg:w-[600px] z-10 ${
                     isLeft ? 'left-0 justify-start' : 'right-0 justify-end'
-                }`}
+                } ${isTop ? "top-0" : "bottom-0"}`}
             >
                 <div className="max-w-full max-h-full w-96 h-96 2xl:w-[600px] 2xl:h-[600px] relative ">
                     <Image
