55655be83c00158b001dce45f392b8f397cbfb6e
Rob <robzlegz@gmail.com>
2022-08-16 09:04:58 +0300

    format

--- frontend/src/components/gallery/GalleryImages.tsx
+++ frontend/src/components/gallery/GalleryImages.tsx
@@ -18,9 +18,10 @@ function GalleryImages() {
     return (
         <div className="mt-2 grid grid-cols-3 place-content-center w-full overflow-hidden sm:grid-cols-5 md:grid-cols-7 lg:grid-cols-10 gap-2">
             {appInfo.files &&
-                appInfo.files.map((file) => file && (
-                    <GalleryImage file={file} key={file._id.$oid} />
-                ))}
+                appInfo.files.map(
+                    (file) =>
+                        file && <GalleryImage file={file} key={file._id.$oid} />
+                )}
         </div>
     );
 }
