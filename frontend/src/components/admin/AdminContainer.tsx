d7019a3c46fe2043e0cf150d8c39e3c5b6601e09
Rob <robzlegz@gmail.com>
2022-08-16 11:07:24 +0300

    responsive admin

--- frontend/src/components/admin/AdminContainer.tsx
+++ frontend/src/components/admin/AdminContainer.tsx
@@ -1,4 +1,8 @@
-import { DocumentIcon, FolderOpenIcon, TrashIcon } from '@heroicons/react/solid';
+import {
+    DocumentIcon,
+    FolderOpenIcon,
+    TrashIcon,
+} from '@heroicons/react/solid';
 import Image from 'next/image';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -27,46 +31,50 @@ function AdminContainer() {
                                 className="flex items-start justify-start w-full mb-2"
                                 key={file.name}
                             >
-                                {!file.name.includes('.png') &&
-                                !file.name.includes('.jpg') &&
-                                !file.name.includes('.gif') &&
-                                !file.name.includes('.jpeg') &&
-                                !file.name.includes('.svg') &&
-                                !file.name.includes('.jfif') &&
-                                !file.name.includes('.webp') ? (
-                                    <div className="flex h-full items-center justify-center w-10">
-                                        <DocumentIcon className="text-white h-8" />
-                                    </div>
-                                ) : (
-                                    <Image
-                                        src={`${BASE_URL}/${file.hash}`}
-                                        alt={file.name}
-                                        draggable={false}
-                                        objectFit="cover"
-                                        width={40}
-                                        height={40}
-                                    />
-                                )}
+                                <div className="w-10 h-10 relative flex items-center justify-center mr-2">
+                                    {!file.name.includes('.png') &&
+                                    !file.name.includes('.jpg') &&
+                                    !file.name.includes('.gif') &&
+                                    !file.name.includes('.jpeg') &&
+                                    !file.name.includes('.svg') &&
+                                    !file.name.includes('.jfif') &&
+                                    !file.name.includes('.webp') ? (
+                                        <div className="flex h-full items-center justify-center w-full">
+                                            <DocumentIcon className="text-white h-8" />
+                                        </div>
+                                    ) : (
+                                        <Image
+                                            src={`${BASE_URL}/${file.hash}`}
+                                            alt={file.name}
+                                            draggable={false}
+                                            objectFit="cover"
+                                            layout='fill'
+                                        />
+                                    )}
+                                </div>
 
-                                <div className="flex-1 flex items-start justify-start h-full ml-2">
-                                    <div className="flex flex-col justify-between items-start h-full w-full max-w-[300px]">
-                                        <p className="text-white">
+                                <div className="flex items-start justify-start h-full flex-1">
+                                    <div className="flex flex-col justify-between items-start h-full flex-1 max-w-[200px] sm:max-w-[400px] pr-2 cursor-pointer">
+                                        <p className="text-white truncate w-full text-left">
                                             {file.name}
                                         </p>
 
-                                        <small className="text-gray-300">
+                                        <small className="text-gray-300 truncate w-full text-left">
                                             {file.hash}
                                         </small>
                                     </div>
-                                    
-                                    <button className="bg-green-500 h-8 w-12 flex items-center justify-center"
-                                        onClick={() =>
-                                            open(`https://46.109.36.103/${file.hash}`)
-                                        }>
-                                        <FolderOpenIcon className='text-white h-5'/>
-                                    </button>
+
                                     <button
-                                        className="bg-red-500 h-8 w-8 flex items-center justify-center"
+                                        className="bg-green-500 h-8 w-10 flex items-center justify-center"
+                                        onClick={() =>
+                                            open(`${BASE_URL}/${file.hash}`)
+                                        }
+                                    >
+                                        <FolderOpenIcon className="text-white h-5" />
+                                    </button>
+
+                                    <button
+                                        className="bg-red-500 h-8 w-10 flex items-center justify-center"
                                         onClick={() =>
                                             deleteFile(file._id.$oid, dispatch)
                                         }
