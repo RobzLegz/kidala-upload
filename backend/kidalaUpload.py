390390ed6b5c0964bdbfd2d1327c5387846564a3
Piparkuucinsh <audaja107@gmail.com>
2022-08-18 23:02:03 +0300

    fix file

--- backend/kidalaUpload.py
+++ backend/kidalaUpload.py
@@ -151,9 +151,8 @@ def upload(**kwargs):
             }
 
             result = dbfiles.insert_one(fileentry)
-            result['_id'] = str(result['_id'])
 
-            return make_response({'msg': "success", 'url': f"https://{SERVER_IP}/{md5hash}", 'hash': md5hash, 'access_token': token, 'file': result}, 201)
+            return make_response({'msg': "success", 'url': f"https://{SERVER_IP}/{md5hash}", 'hash': md5hash, 'access_token': token, 'file': fileentry.update({'_id': str(result.inserted_id)})}, 201)
 
         else:
 
@@ -166,9 +165,7 @@ def upload(**kwargs):
 
             result = dbfiles.insert_one(fileentry)
 
-            result['_id'] = str(result['_id'])
-
-            return make_response({'msg': "success", 'url': f"https://{SERVER_IP}/{md5hash}", 'hash': md5hash, 'file': result}, 201)
+            return make_response({'msg': "success", 'url': f"https://{SERVER_IP}/{md5hash}", 'hash': md5hash, 'file': fileentry.update({'_id': str(result.inserted_id)})}, 201)
 
     return make_response({'msg': "failed"}, 500)
 
