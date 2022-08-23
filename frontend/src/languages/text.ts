c1db968c430762d4ea9937cbf0d3a380436bb794
Rob <robzlegz@gmail.com>
2022-08-23 10:42:57 +0300

    langs

--- frontend/src/languages/text.ts
+++ frontend/src/languages/text.ts
@@ -1,17 +1,61 @@
 export const supportedLanguages = [
     {
-        language: "Latvian",
-        short: "LV",
-        flag: "https://www.worldometers.info/img/flags/small/tn_lg-flag.gif"
+        language: 'Latvian',
+        short: 'LV',
+        flag: 'https://www.worldometers.info/img/flags/small/tn_lg-flag.gif',
+        lang: 'lv',
     },
     {
-        language: "English (UK)",
-        short: "EN (UK)",
-        flag: "https://www.worldometers.info/img/flags/small/tn_uk-flag.gif"
+        language: 'English (UK)',
+        short: 'EN (UK)',
+        flag: 'https://www.worldometers.info/img/flags/small/tn_uk-flag.gif',
+        lang: 'en',
     },
     {
-        language: "English (USA)",
-        short: "EN (USA)",
-        flag: "https://www.worldometers.info/img/flags/small/tn_us-flag.gif"
-    }
-]
+        language: 'English (USA)',
+        short: 'EN (USA)',
+        flag: 'https://www.worldometers.info/img/flags/small/tn_us-flag.gif',
+        lang: 'en',
+    },
+];
+
+export const translatedText = {
+    lv: {
+        home: {
+            selectFile: 'izvēlieties failu',
+            upload: 'augšupielādēt',
+            maxSize: '(max faila izmērs 1mb)',
+        },
+        navigation: {
+            gallery: 'Galerija',
+            myFiles: 'Mani faili',
+            home: 'Sākums',
+        },
+        gallery: {
+            contact: 'Kontaktē reklāmdevēju:',
+            download: 'Lejuplādēt',
+            copyLink: 'Kopēt saiti',
+            showNonImageFiles: 'Rādīt failus kas nav bildes',
+            seen: 'Redzēts:',
+        },
+    },
+    en: {
+        home: {
+            selectFile: 'select file',
+            upload: 'upload',
+            maxSize: '(max file size 1mb)',
+        },
+        navigation: {
+            gallery: 'Galley',
+            myFiles: 'My files',
+            home: 'Home',
+        },
+        gallery: {
+            contact: 'Contact advertiser:',
+            download: 'Download',
+            copyLink: 'Copy url',
+            showNonImageFiles: 'Show non image files',
+            seen: 'Seen:',
+        },
+    },
+};
