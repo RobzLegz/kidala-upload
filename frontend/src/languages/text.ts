c43169b678caeae732879e6092d56c9a42818648
Rob <robzlegz@gmail.com>
2022-08-23 10:30:50 +0300

    supported langs

---
+++ frontend/src/languages/text.ts
@@ -0,0 +1,17 @@
+export const supportedLanguages = [
+    {
+        language: "Latvian",
+        short: "LV",
+        flag: "https://www.worldometers.info/img/flags/small/tn_lg-flag.gif"
+    },
+    {
+        language: "English (UK)",
+        short: "EN (UK)",
+        flag: "https://www.worldometers.info/img/flags/small/tn_uk-flag.gif"
+    },
+    {
+        language: "English (USA)",
+        short: "EN (USA)",
+        flag: "https://www.worldometers.info/img/flags/small/tn_us-flag.gif"
+    }
+]
