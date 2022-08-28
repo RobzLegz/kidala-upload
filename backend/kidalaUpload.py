01638f2c2280f3aeb536e6de85284c1ff235b10e
Piparkuucinsh <audaja107@gmail.com>
2022-08-29 02:51:09 +0300

    add docker

--- backend/kidalaUpload.py
+++ backend/kidalaUpload.py
@@ -21,7 +21,9 @@ app.config['MAX_CONTENT_LENGTH'] = 100 * 1000 * 1000
 
 UPLOAD_FOLDER = Path(app.root_path) / "files"
 SERVER_IP = os.getenv('SERVER_IP')
-MONGO_DB_LINK = os.getenv('MONGODBLINK')
+MONGO_USERNAME = os.getenv('MONGO_INITDB_ROOT_USERNAME')
+MONGO_PASSWORD = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
+MONGO_DB_LINK = f'mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@mongodb'
 app.config['ADMIN_TOKEN'] = os.getenv('ADMIN_TOKEN')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
