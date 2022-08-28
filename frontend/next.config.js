78874eb5ce122c310e8d0d938d4fc8c4f1d8c3e5
Rob <robzlegz@gmail.com>
2022-08-28 23:34:12 +0300

    domain config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-        domains: ['46.109.36.103', 'www.worldometers.info'],
+        domains: ['46.109.36.103', 'www.worldometers.info', 'cdn.kidala.life'],
     },
 };
