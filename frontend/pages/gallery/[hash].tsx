6ca8885a013a90d0ffc5b70efc9c65d8bd87119c
Rob <robzlegz@gmail.com>
2022-08-29 21:54:01 +0300

    test

--- frontend/pages/gallery/[hash].tsx
+++ frontend/pages/gallery/[hash].tsx
@@ -77,9 +77,8 @@ export default function Home(
                             : '/images/janisbataragsuzliso.png'
                     }
                 />
-                <meta property="twitter:card" content="summary" />
-                {/* <meta property="og:image:width" content="4608" /> */}
-                {/* <meta property="og:image:height" content="3456" /> */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="og:image:width" content="4608" />
                 <meta property="og:site_name" content="kidala.life" />
                 <meta property="og:type" content="website" />
             </Head>
