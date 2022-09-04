08690d4265253fc216840f528b26002ff97b05a9
MareksNo <ulmanismareks@gmail.com>
2022-09-04 16:07:05 +0100

    Added allfiles view.

--- kebab/endpoints/files.py
+++ kebab/endpoints/files.py
@@ -5,16 +5,26 @@ from json import JSONEncoder, dumps
 
 from ..database import File, db
 
+class Page(BaseModel):
+    files: list[File]
+    total: int
+    total_page: int
+    page: int
+    limit: int
+
 router = APIRouter(
     prefix="/files",
     tags=["files"],
     responses={404: {"description": "Not found"}},
 )
 
-@router.get("/allfiles", response_model=list[File])
-async def all_files():
-    files = list(db.files.find())
-    return files
+@router.get("/allfiles", response_model=Page)
+async def all_files(page: int = 0, limit: int = 20):
+    if page >= 0 and limit >= 0:
+        files = list(db.files.find().skip(page * limit).limit(limit)) 
+        return Page(files=files, page=page, limit=limit, total=db.files.count_documents({}), total_page=files.count({}))
+    else:
+        return []
 
 @router.get('/file', response_model=File)
 async def get_file(file_hash: str):
