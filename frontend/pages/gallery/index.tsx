850a2dca820e4626bf1b9acb3a722026b6528312
RobzLegz <robzlegz@gmail.com>
2022-09-08 14:38:05 +0300

    ssr for gallery

--- frontend/pages/gallery/index.tsx
+++ frontend/pages/gallery/index.tsx
@@ -1,6 +1,13 @@
 import Head from 'next/head';
 import CheckAuth from '../../src/hooks/CheckAuth';
 import dynamic from 'next/dynamic';
+import { PageComponent } from '../../src/types/PageComponent';
+import { FileInterface } from '../../src/interfaces/file';
+import { sortFiles } from '../../src/utils/sortFiles';
+import { LIST_FILES } from '../../src/requests/routes';
+import { useEffect } from 'react';
+import { AppInfo, selectApp, setFiles } from '../../src/redux/slices/appSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
 const GalleryContainer = dynamic(
     () => import('../../src/components/gallery/GalleryContainer')
@@ -9,7 +16,21 @@ const LanguageSelector = dynamic(
     () => import('../../src/components/language/LanguageSelector')
 );
 
-export default function Gallery() {
+interface GalleryProps {
+    files: FileInterface[];
+}
+
+const Gallery: PageComponent<GalleryProps> = ({ files }) => {
+    const dispatch = useDispatch();
+
+    const appInfo: AppInfo = useSelector(selectApp);
+
+    useEffect(() => {
+        if (!appInfo.files) {
+            dispatch(setFiles(files));
+        }
+    }, []);
+
     return (
         <div className="page">
             <Head>
@@ -21,10 +42,28 @@ export default function Gallery() {
             </Head>
 
             <GalleryContainer />
-            
+
             <CheckAuth />
 
             <LanguageSelector />
         </div>
     );
-}
+};
+
+Gallery.getInitialProps = async () => {
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const res = await fetch(LIST_FILES, requestOptions);
+    const resJson: FileInterface[] = await res.json();
+
+    return {
+        files: resJson,
+    };
+};
+
+export default Gallery;
