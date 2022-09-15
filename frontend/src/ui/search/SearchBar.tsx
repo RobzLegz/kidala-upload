ec11f922a03439223156ce13a8689b027538c515
Rob <robzlegz@gmail.com>
2022-09-15 08:48:46 +0300

    storybook setup

---
+++ frontend/src/ui/search/SearchBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export interface SearchBarProps
+    extends React.ComponentPropsWithoutRef<'input'> {
+    inputClassName?: string;
+    isLoading?: boolean;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({
+    inputClassName = '',
+    isLoading = false,
+    ...props
+}) => {
+    return (
+        <div
+            className={`items-center flex w-full bg-primary-700 text-primary-300 transition duration-200 ease-in-out focus-within:text-primary-100 rounded-lg ${props.className}`}
+        >
+            asdasd
+        </div>
+    );
+};
+
+export default SearchBar;
