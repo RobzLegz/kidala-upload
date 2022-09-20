bc9af680b87da4dbf1346f8f90be84ec9bae89d3
RobzLegz <robzlegz@gmail.com>
2022-09-20 21:13:10 +0300

    auth pages

--- frontend/pages/new/login.tsx
+++ frontend/pages/new/login.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Login from '../../src/modules/pages/Login';
 
 export default Login;
--- frontend/src/modules/pages/Register.tsx
+++ frontend/src/modules/pages/Register.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 import AuthForm from '../../ui/auth/AuthForm';
 import HomeNav from '../../ui/navigation/HomeNav';
 import PageModule from '../PageModule';
 
-const Login = () => {
+const Register = () => {
     return (
         <PageModule
-            title="Login"
+            title="Register"
             description="Kidala file upload. Free file hosting, unlimited uploads"
             className="justify-center h-screen"
         >
             <HomeNav />
 
-            <AuthForm />
+            <AuthForm register />
         </PageModule>
     );
-}
+};
 
-export default Login
+export default Register;
