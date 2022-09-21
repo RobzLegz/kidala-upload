4c2a63542f86c670bb81f1981dff478830fb2908
Piparkuucinsh <audaja107@gmail.com>
2022-09-21 22:19:51 +0300

    update existing user when registering

--- kebab/endpoints/users.py
+++ kebab/endpoints/users.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Form
 from fastapi.security import OAuth2PasswordRequestForm 
 
 from ..database import User, File, db, Token, User
-from ..auth import get_current_user, authenticate_user, create_access_token, get_password_hash
+from ..auth import get_current_user, authenticate_user, create_access_token, get_password_hash, get_potential_user
 
 router = APIRouter(
     prefix="/api/v1/users",
@@ -25,22 +25,34 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     return Token(access_token=access_token, token_type='bearer')
 
 @router.post("/register", response_model=User)
-async def register_user(username: str = Form(), password: str = Form(), email: str = Form()):
-    emailq = db.users.find_one({"email": email})
+async def register_user(username: str = Form(), password: str = Form(), email: str = Form(), current_user: User = Depends(get_potential_user)):
+    emailq = db.users.find_one({"email": email.lower()})
     if emailq != None:
         return {"err": "User with this e-mail already exists."}
+
+    if not username.isalnum():
+        return {"err": "Username should only contain alphanumeric characters"}
+
     usernameq = db.users.find_one({"username": username})
     if usernameq != None:
         return {"err": "User with this username already exists."}
 
     hashed_pass = get_password_hash(password)
 
-    user = User(email=email, username=username, password=hashed_pass, role='default')
+    if current_user.id == None:
+        user = User(email=email, username=username, password=hashed_pass, role='default')
 
-    db.users.insert_one(user.dict(exclude={'id'}))
+        db.users.insert_one(user.dict(exclude={'id'}))
+
+    else:
+        user = db.users.find_one({"_id": current_user.id})
+        user["email"] = email
+        user["password"] = hashed_pass
+        user["username"] = username
+
+        db.users.replace_one({'_id': current_user.id}, user)
 
     return user
-    
 
 @router.get("/me", response_model=User)
 async def get_own_user(current_user: User = Depends(get_current_user)):
