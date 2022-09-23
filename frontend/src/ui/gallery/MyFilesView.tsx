f7321a932ef183e681276dd9e6908faf074cb1c2
Rob <robzlegz@gmail.com>
2022-09-23 19:55:54 +0300

    aaaaaaaaa

--- frontend/src/ui/gallery/MyFilesView.tsx
+++ frontend/src/ui/gallery/MyFilesView.tsx
@@ -14,25 +14,34 @@ const MyFilesView = () => {
             <div className="w-11/12 flex flex-col items-center justify-start max-w-[800px]">
                 {userInfo.myFiles &&
                     userInfo.myFiles.map((myFile, i) => (
-                        <div className="w-full bg-primary-800 border border-primary-700 p-2 rounded-lg flex items-start justify-start" key={i}>
-                            {detectFileType(myFile.name) === 'image' ? (
-                                <Image
-                                    src={generateFileUrl(
-                                        myFile.hash,
-                                        myFile.name
-                                    )}
-                                    width={100}
-                                    height={60}
-                                    objectFit="cover"
-                                />
-                            ) : (
-                                <GalleryNonImage filename={myFile.name} />
-                            )}
+                        <div
+                            className="w-full bg-primary-800 border border-primary-700 p-2 rounded-lg flex items-start justify-start"
+                            key={i}
+                        >
+                            <div className="w-28 mr-2">
+                                {detectFileType(myFile.name) === 'image' ? (
+                                    <Image
+                                        src={generateFileUrl(
+                                            myFile.hash,
+                                            myFile.name
+                                        )}
+                                        width={112}
+                                        height={80}
+                                        objectFit="cover"
+                                    />
+                                ) : (
+                                    <GalleryNonImage filename={myFile.name} />
+                                )}
+                            </div>
 
-                            <div className="h-full flex flex-col ml-2">
-                                <strong className='text-white'>{myFile.name}</strong>
+                            <div className="h-full flex flex-col flex-1">
+                                <strong className="text-white">
+                                    {myFile.name}
+                                </strong>
 
-                                <p className='text-primary-100'>{myFile.description}</p>
+                                <p className="text-primary-100">
+                                    {myFile.description}
+                                </p>
                             </div>
                         </div>
                     ))}
