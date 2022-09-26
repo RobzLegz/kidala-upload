124b8ca2fcaf055832e897dae858951dfe542157
Rob <robzlegz@gmail.com>
2022-09-26 08:11:46 +0300

    mb formtating

--- frontend/src/utils/formatFileSize.ts
+++ frontend/src/utils/formatFileSize.ts
@@ -1,5 +1,5 @@
 export const formatFileSize = (size: number) => {
-    const formated = Math.floor(size / 10000) / 10;
+    const formated = Math.floor(size / 100000) / 10;
 
     return formated;
 };
