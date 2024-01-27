c1c6eb44b549b67ee6dc88f72cf520973d071a2e
Rob <robzlegz@gmail.com>
2024-01-28 00:24:36 +0200

    asd

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <meta name="google-adsense-account" content="ca-pub-3564006936877547" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link
                     rel="preconnect"
