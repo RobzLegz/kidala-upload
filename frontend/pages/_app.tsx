37db6cb60af50ddfe8f0d8f3b891be2ce2b92d63
Rob <robzlegz@gmail.com>
2024-01-28 00:20:01 +0200

    stafs

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,18 @@
-import { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import store from "../src/redux/app/store";
-import "../src/styles/globals.css";
+import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import store from '../src/redux/app/store';
+import '../src/styles/globals.css';
+import Script from 'next/script';
 
 function App({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
+            <Script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3564006936877547"
+                strategy="afterInteractive"
+                crossOrigin="anonymous"
+            />
             <Component {...pageProps} />
         </Provider>
     );
