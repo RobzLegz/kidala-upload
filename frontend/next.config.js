292511bcfb297ca1ac8f5fe437f9385987a23c7c
Rob <robzlegz@gmail.com>
2024-02-01 22:25:07 +0200

    stafs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,8 +4,9 @@ module.exports = {
             '46.109.36.103',
             'www.worldometers.info',
             'cdn.kidala.life',
+            'api.kidala.life',
             'localhost',
-            '78-84-242-254.nip.io'
+            '78-84-242-254.nip.io',
         ],
     },
 };
