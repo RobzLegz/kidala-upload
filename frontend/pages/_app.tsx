ac0ffca1052ab17e8a46a2e30454bb9b52a9e3a1
Piparkuucinsh <audaja107@gmail.com>
2024-10-31 14:12:27 +0200

    remove google ads

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,12 +7,12 @@ import Script from 'next/script';
 function App({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
-            <Script
+            {/* <Script
                 async
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3564006936877547"
                 strategy="afterInteractive"
                 crossOrigin="anonymous"
-            />
+            /> */}
             <Component {...pageProps} />
         </Provider>
     );
